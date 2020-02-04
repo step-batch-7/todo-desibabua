e60408828b88290e51e58f5c473f15fa8573b1ea
ayush <58028488+desibabua@users.noreply.github.com>
2020-02-04 10:59:13 +0530

    added dustbinImage

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,15 +1,51 @@
-const appendItemToPage = function(content) {
-  const lists = document.querySelector('#lists');
+const createContainer = function() {
   const item = document.createElement('div');
   item.className = 'container';
+  return item;
+};
+
+const createCheckBox = function() {
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
+  return checkbox;
+};
+
+const createDustbin = function() {
+  const img = document.createElement('img');
+  img.setAttribute(
+    'src',
+    'https://www.flaticon.com/premium-icon/icons/svg/484/484662.svg'
+  );
+  img.setAttribute('width', '16px');
+  img.setAttribute('height', '16px');
+  img.className = 'dustbin';
+  return img;
+};
+
+const appendItemToPage = function(content) {
+  const lists = document.querySelector('#lists');
+  // const item = document.createElement('div');
+  // item.className = 'container';
+  // const checkbox = document.createElement('input');
+  // checkbox.setAttribute('type', 'checkbox');
+  const container = createContainer();
   const title = document.createElement('p');
   title.innerText = content.title;
-  item.appendChild(checkbox);
-  item.appendChild(title);
-  lists.appendChild(item);
+  container.appendChild(createCheckBox());
+  container.appendChild(title);
+  container.appendChild(createDustbin());
+  lists.appendChild(container);
 };
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* <img
+  src="https://www.flaticon.com/premium-icon/icons/svg/484/484662.svg"
+  alt=""
+  width="16px"
+  height="16px"
+/>; */
+}
 
 const getInputToAppend = function() {
   const input = document.querySelector('#input');
